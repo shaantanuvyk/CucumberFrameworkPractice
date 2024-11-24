9b0cef4b8f88ff02a4baeea31df65ff606a034b8
shaantanuvyk <shaantanukarambalkar@gmail.com>
2024-11-24 08:31:06 +0530

    first commit to add files

---
+++ src/main/java/com/Practice/CucumberFrameworkPractice/App.java
@@ -0,0 +1,13 @@
+package com.Practice.CucumberFrameworkPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
