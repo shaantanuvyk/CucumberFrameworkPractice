bacb9d56ac7f226c302c4cffbff18339c177344f
shaantanuvyk <shaantanukarambalkar@gmail.com>
2024-11-25 09:43:29 +0530

    3

--- src/test/java/com/Practice/CucumberFrameworkPractice/Test1.java
+++ src/test/java/com/Practice/CucumberFrameworkPractice/Test1.java
@@ -2,8 +2,9 @@ package com.Practice.CucumberFrameworkPractice;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
 
 	}
 
