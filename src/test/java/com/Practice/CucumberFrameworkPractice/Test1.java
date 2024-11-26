d6d5067053bb046a903ef1c22a203ce721f075a4
shaantanuvyk <shaantanukarambalkar@gmail.com>
2024-11-26 09:23:15 +0530

    t3

--- src/test/java/com/Practice/CucumberFrameworkPractice/Test1.java
+++ src/test/java/com/Practice/CucumberFrameworkPractice/Test1.java
@@ -5,7 +5,7 @@ public class Test1 {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World");
-		System.out.println("This is a test 1 tested explaine");
+		System.out.println("This is a test 1 tested explained");
 	
 	}
 
